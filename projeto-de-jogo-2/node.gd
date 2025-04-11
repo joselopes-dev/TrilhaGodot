bf3609c95c3dbb56b618b0342977ee1e6f86937c
jose maria <josel6449@gmail.com>
2025-04-11 16:26:18 -0400

    colisao de blocos
    Código colisão de blocos

--- projeto-de-jogo-2/node.gd
+++ projeto-de-jogo-2/node.gd
@@ -1,11 +1,20 @@
 extends Node
 
+@export var object_templates: Array[PackedScene]
+
+#extends Node
+
 func _input(event: InputEvent) -> void:
 	#input no botão e um evento de click
 	if event is InputEventMouseButton:
 		if event.button_index == 1:
 			if event.pressed:
-				spaw_objet ()
+				spaw_object(event.position)
 				
-func spaw_objet () -> void:
-	pass
+func spaw_object(position: Vector2) -> void:
+	var index: int = randi_range(0, object_templates.size() -1)
+	var object_template = object_templates[index]
+	var object = object_template.instantiate()
+	
+	add_child(object)		
+		
